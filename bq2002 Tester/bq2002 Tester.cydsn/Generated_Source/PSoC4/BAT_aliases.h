c633e1138581ea8f96471371cbc193c5b8fe6f28
Moses Wan <wanmoses1@gmail.com>
2015-07-02 21:37:54 +1000

    Commit Ignored files Test

---
+++ bq2002 Tester/bq2002 Tester.cydsn/Generated_Source/PSoC4/BAT_aliases.h
@@ -0,0 +1,39 @@
+/*******************************************************************************
+* File Name: BAT.h  
+* Version 2.10
+*
+* Description:
+*  This file contains the Alias definitions for Per-Pin APIs in cypins.h. 
+*  Information on using these APIs can be found in the System Reference Guide.
+*
+* Note:
+*
+********************************************************************************
+* Copyright 2008-2014, Cypress Semiconductor Corporation.  All rights reserved.
+* You may use this file only in accordance with the license, terms, conditions, 
+* disclaimers, and limitations in the end user license agreement accompanying 
+* the software package with which this file was provided.
+*******************************************************************************/
+
+#if !defined(CY_PINS_BAT_ALIASES_H) /* Pins BAT_ALIASES_H */
+#define CY_PINS_BAT_ALIASES_H
+
+#include "cytypes.h"
+#include "cyfitter.h"
+#include "cypins.h"
+
+
+/***************************************
+*              Constants        
+***************************************/
+#define BAT_0		(BAT__0__PC)
+#define BAT_0_PS		(BAT__0__PS)
+#define BAT_0_PC		(BAT__0__PC)
+#define BAT_0_DR		(BAT__0__DR)
+#define BAT_0_SHIFT	(BAT__0__SHIFT)
+
+
+#endif /* End Pins BAT_ALIASES_H */
+
+
+/* [] END OF FILE */
